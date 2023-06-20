209f48dff5691a68fb87705d3d44236a27997241
Gustavo Gomez <gustavogomez092@gmail.com>
2023-06-20 15:00:19 -0600

    new dev & prod workflows on vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,67 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import path, { resolve } from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/main.js'),
-      name: 'vue-wp',
-      // the proper extensions will be added
-      fileName: 'vue-wp',
-    },
-  },
-  define: {
-    'process.env': {}
-  },
-  plugins: [vue()],
-  server: {
-    port: 5173
+let UrlArray
+if (process.platform === 'win32') {
+  UrlArray = __dirname.split('\\')
+} else {
+  UrlArray = __dirname.split('/')
+}
+const finalPath = UrlArray.slice(Math.max(UrlArray.length - 3, 0)).join('/')
+
+export default defineConfig(({ command, mode }) => {
+  if (mode === 'development') {
+    return {
+      base: `/${finalPath}/`,
+      resolve: {
+        alias: [
+          { find: '@', replacement: path.resolve(__dirname, 'src') }
+        ]
+      },
+      build: {
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: resolve(__dirname, 'src/main.js'),
+          name: 'vue-wp',
+          // the proper extensions will be added
+          fileName: 'vue-wp',
+        },
+      },
+      define: {
+        'process.env': {}
+      },
+      plugins: [vue()],
+      server: {
+        port: 5173
+      }
+    }
+  }
+  if (mode === 'production') {
+    return {
+      base: `/${finalPath}/dist`,
+      resolve: {
+        alias: [
+          { find: '@', replacement: path.resolve(__dirname, 'src') }
+        ]
+      },
+      build: {
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: resolve(__dirname, 'src/main.js'),
+          name: 'vue-wp',
+          // the proper extensions will be added
+          fileName: 'vue-wp',
+        },
+      },
+      define: {
+        'process.env': {}
+      },
+      plugins: [vue()],
+      server: {
+        port: 5173
+      }
+    }
   }
 })
+
